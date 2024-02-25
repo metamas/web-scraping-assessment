3718ea8a483ef7848755996e4df12d797431ec61
Metamas <2520253+metamas@users.noreply.github.com>
2024-02-25 13:03:14 -0800

    Extend user prompt flow

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
 import puppeteer from "puppeteer";
 
-import promptForCredentials from "./prompt.js";
-import { amazonSignIn, amazonOrderHistory } from "./amazon.js";
+import { promptForCredentials, promptForAction, promptForLimit } from "./prompt.js";
+import { amazonSignIn, amazonOrderHistory, OrderData } from "./amazon.js";
 
 async function main() {
   const credentials = await promptForCredentials("Amazon");
+  const action = await promptForAction();
   // Use headful mode in case manual MFA is required
   const browser = await puppeteer.launch({ headless: false, defaultViewport: null });
-
   await amazonSignIn(browser, credentials);
-  const orders = await amazonOrderHistory(browser);
+
+  let orders: OrderData[] = [];
+  if (action === "Retrieve recent orders") {
+    const limit = await promptForLimit();
+    orders = await amazonOrderHistory(browser, limit);
+  }
+
   console.dir({ count: orders.length, orders }, { depth: null, colors: true });
   await browser.close();
 }
--- src/prompt.ts
+++ src/prompt.ts
@@ -1,36 +1,94 @@
-import inquirer from 'inquirer';
+import inquirer from "inquirer";
 
-async function promptForCredentials(websiteName: string): Promise<{ username: string, password: string }>{
+async function promptForCredentials(websiteName: string): Promise<{ username: string; password: string }> {
   const questions = [
     {
-      type: 'input',
-      name: 'username',
+      type: "input",
+      name: "username",
       message: `Enter your ${websiteName} username:`,
-      validate: function(value: string) {
+      validate: function (value: string) {
         if (value.length) {
           return true;
         } else {
           return `Please enter your ${websiteName} username.`;
         }
-      }
+      },
     },
     {
-      type: 'password',
-      name: 'password',
+      type: "password",
+      name: "password",
       message: `Enter your ${websiteName} password:`,
-      mask: '*',
-      validate: function(value: string) {
+      mask: "*",
+      validate: function (value: string) {
         if (value.length) {
           return true;
         } else {
           return `Please enter your ${websiteName} password.`;
         }
-      }
-    }
+      },
+    },
   ];
 
   const credentials = await inquirer.prompt(questions);
   return credentials;
 }
 
-export default promptForCredentials;
+async function promptForAction(): Promise<string> {
+  const question = {
+    type: "list",
+    name: "action",
+    message: "What would you like to do?",
+    choices: ["Retrieve recent orders", "Search for orders"],
+  };
+
+  const { action } = await inquirer.prompt([question]);
+  return action;
+}
+
+async function promptForLimit(): Promise<number> {
+  const question = {
+    type: "number",
+    name: "limit",
+    message: "Give a maximum number of recent orders to retrieve.",
+    default: 10,
+    validate: function (value: number) {
+      const valid = !isNaN(value);
+      return valid || "Please enter a number.";
+    },
+  };
+
+  const { limit } = await inquirer.prompt([question]);
+  return parseInt(limit);
+}
+
+async function promptForSearches(): Promise<string> {
+  const question = {
+    type: "input",
+    name: "searches",
+    message: "Give a list of searches to perform, separated by commas.",
+    validate: function (value: string) {
+      const terms = value
+        .split(",")
+        .map((t) => t.trim())
+        .filter((t) => t);
+      const valid = terms.length > 0;
+      return valid || "Please enter at least one search string.";
+    },
+  };
+
+  const { searches } = await inquirer.prompt([question]);
+  return searches;
+}
+
+async function promptForContinue(): Promise<boolean> {
+  const question = {
+    type: "confirm",
+    name: "again",
+    message: "Would you like to continue?",
+  };
+
+  const { again } = await inquirer.prompt([question]);
+  return again;
+}
+
+export { promptForCredentials, promptForAction, promptForSearches, promptForLimit, promptForContinue };
